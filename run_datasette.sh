ed13863ea276ab06214ef157b920bcecd81d3e60
Mingxun Wang <mwang87@gmail.com>
2025-08-14 21:54:03 -0700

    updating max rows

--- run_datasette.sh
+++ run_datasette.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 datasette -p 5234 -h 0.0.0.0 \
---setting max_returned_rows 50000 \
+--setting max_returned_rows 1000000 \
 --setting sql_time_limit_ms 60000 \
 --setting max_csv_mb 0 --immutable /app/database/masst_records_copy.sqlite
