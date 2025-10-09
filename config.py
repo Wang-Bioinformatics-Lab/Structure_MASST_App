7d70692c63f4f939271aedf323f3599b72200101
YasinEl <yelabiead@ucsd.edu>
2025-10-08 21:45:56 -0700

    absolute sqlite path

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-PATH_TO_SQLITE = "app/database/masst_records.sqlite"
+PATH_TO_SQLITE = "/app/database/masst_records.sqlite"
 MASSTRECORDS_ENDPOINT =  "https://masst-records.gnps2.org/masst_records" 
 MASSTRECORDS_TIMEOUT = 1000
 MASSTRECORDS_ROWLIMIT = 1000000
