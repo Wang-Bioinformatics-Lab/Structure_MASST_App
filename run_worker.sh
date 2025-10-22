b301d8bcfaee5b671c24599daa91dea76fe13049
YasinEl <yelabiead@ucsd.edu>
2025-10-22 16:30:06 -0700

    recovered

--- run_worker.sh
+++ run_worker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source activate StructureMASST_env
-celery -A tasks worker --loglevel=INFO --concurrency=8
+celery -A tasks worker --loglevel=INFO --autoscale=10,1  --max-tasks-per-child=5
